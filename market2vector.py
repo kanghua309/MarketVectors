63a1c1aa5b72d88271bdb40dfaac8b64f0a7ed06
kanghua <kanghua151@gmail.com>
2017-07-19 20:16:12 +0800

    ....

--- market2vector.py
+++ market2vector.py
@@ -4,7 +4,7 @@ import os
 import sys
 import numpy as np
 import matplotlib
-matplotlib.use('Agg')
+#matplotlib.use('Agg')
 from matplotlib.pyplot import plot,savefig
 pd.set_option('display.width', 800)
 
@@ -46,11 +46,15 @@ Res = make_inputs(filepath)
 #print Res.corr()
 
 Final = pd.DataFrame()
+idx=0
 for f in os.listdir(datapath):
     filepath = os.path.join(datapath,f)
     if filepath.endswith('.csv'):
         Res = make_inputs(filepath)
         Final = Final.append(Res)
+    idx += 1
+    if idx == 10:
+        break;
 print Final.head(10)
 print "-" * 20,"Muti-stock table","-" * 20
 
@@ -126,18 +130,18 @@ print Labeled[['return','act_return']].cumsum().head(10)
 print "-" * 20,"Return and acture return cumsum","-" * 20
 
 test_size=600
+from sklearn import linear_model
+from sklearn.metrics import classification_report,confusion_matrix
 
-'''
+
 print "*" * 50,"Data prepare over and todo basic predict","*" * 50
 
-from sklearn import linear_model
 logreg = linear_model.LogisticRegression(C=1e5)
 res = logreg.fit(InputDF[:-test_size],Labeled['multi_class'][:-test_size]) #取除了最后600个外的数据做测试集,multi class是按收益排序分段泛化的结果集合
 print Labeled['multi_class'][:-test_size].head(10)
 print InputDF[:-test_size].head(10)
 print "-" * 20, "Input & target sample for log regress", "-" * 20
 
-from sklearn.metrics import classification_report,confusion_matrix #验证？
 print(classification_report(Labeled['multi_class'][-test_size:],res.predict(InputDF[-test_size:]))) #使用最后600个做测试
 print "-" * 20,"lassification report","-" * 20
 print(confusion_matrix(Labeled['multi_class'][-test_size:],res.predict(InputDF[-test_size:])))
@@ -145,7 +149,7 @@ print res.predict(InputDF)
 print "-" * 20,"Confusion matrix 1","-" * 20
 Labeled['predicted_action'] = list(map(lambda x: -1 if x <5 else 0 if x==5 else 1,res.predict(InputDF))) #分三段
 print Labeled['predicted_action'].head(10)
-print "-" * 20,"redicted_action","-" * 20
+print "-" * 20,"Predicted action","-" * 20
 print(confusion_matrix(Labeled['class'][-test_size:],Labeled['predicted_action'][-test_size:]))
 print "-" * 20,"Confusion matrix 2","-" * 20
 Labeled['pred_return'] = Labeled['predicted_action'] * Labeled['return']
@@ -158,9 +162,9 @@ print "-" * 20,"Predict return & act_return & pred_return cumsum ","-" * 20
 
 Res[0] =0
 Res.plot()
-'''
 
-print "*" * 50,"Data prepare over and todo DNN predict","*" * 50
+
+print "*" * 50,"Training a basic feed forward network","*" * 50
 
 import tensorflow as tf
 from  tensorflow.contrib.learn.python.learn.estimators.dnn  import DNNClassifier
@@ -171,7 +175,7 @@ dropout=0.2
 hidden_1_size = 1000
 hidden_2_size = 250
 num_classes = Labeled.tf_class.nunique()
-NUM_EPOCHS=100
+NUM_EPOCHS=100 #
 BATCH_SIZE=50
 lr=0.0001
 
@@ -181,7 +185,6 @@ NUM_TRAIN_BATCHES = int(len(train[0])/BATCH_SIZE)
 NUM_VAL_BATCHES = int(len(val[1])/BATCH_SIZE)
 
 print len(InputDF)
-
 
 class Model():
     def __init__(self):
@@ -206,7 +209,7 @@ class Model():
                 inputs=layer_2,
             )
         with tf.variable_scope("loss"):
-            self.losses = tf.nn.sparse_softmax_cross_entropy_with_logits(self.logits, self.target_data)
+            self.losses = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.logits, labels=self.target_data)#???
             mask = (1 - tf.sign(1 - self.target_data))  # Don't give credit for flat days
             mask = tf.cast(mask, tf.float32)
             self.loss = tf.reduce_sum(self.losses)
@@ -228,7 +231,7 @@ with tf.Graph().as_default():
     input_ = train[0]
     target = train[1]
     with tf.Session() as sess:
-        init = tf.initialize_all_variables()
+        init = tf.global_variables_initializer()
         sess.run([init])
         epoch_loss = 0
         for e in range(NUM_EPOCHS):
@@ -283,3 +286,206 @@ with tf.Graph().as_default():
             else:
                 final_probs = np.concatenate((final_probs, probs), axis=0)
         prediction_conf = final_probs[np.argmax(final_probs, 1)]
+
+print "*" * 20,"Train over","*" * 20
+Result = Labeled[-test_size:].copy()
+Result['nn_pred'] = final_preds
+Result['mod_nn_prod'] = list(map(lambda x: -1 if x <5 else 0 if x==5 else 1,final_preds))
+Result['nn_ret'] = Result.mod_nn_prod*Result['return']
+Res = Result[-test_size:][['return','act_return','nn_ret','pred_return']].cumsum()
+print "*" * 20,"NN predict return","*" * 20
+
+#Res = Result[-test_size:][['return','act_return','pred_return','nn_ret']].cumsum()
+#Res = (1+Result[-test_size:][['return','act_return','nn_ret','pred_return']]).cumprod()
+Res[0] =0
+Res.plot(secondary_y='act_return')
+print(confusion_matrix(Result['class'],Result['mod_nn_prod']))
+print "-" * 20,"Confusion matrix","-" * 20
+
+print(classification_report(Result['class'],Result['mod_nn_prod']))
+cm = pd.DataFrame(confusion_matrix(Result['multi_class'],Result['nn_pred']))
+print "-" * 20,"Classification report","-" * 20
+#sns.heatmap(cm.div(cm.sum(1)))
+Result[Result.multi_class==6]['return'].hist()
+print(classification_report(Result['multi_class'],Result['nn_pred']))
+#Result.hist(by='multi_class',column='return',sharex=True) ## fix higer pandas
+
+
+print "*" * 50,"Training a rnn network","*" * 50
+
+from tensorflow.contrib.layers.python.layers.initializers import xavier_initializer
+RNN_HIDDEN_SIZE=100
+FIRST_LAYER_SIZE=1000
+SECOND_LAYER_SIZE=250
+NUM_LAYERS=2
+BATCH_SIZE=50
+NUM_EPOCHS=20
+lr=0.0003
+NUM_TRAIN_BATCHES = int(len(train[0])/BATCH_SIZE)
+NUM_VAL_BATCHES = int(len(val[1])/BATCH_SIZE)
+ATTN_LENGTH=30
+beta=0
+
+
+class RNNModel():
+    def __init__(self):
+        global_step = tf.contrib.framework.get_or_create_global_step()
+        self.input_data = tf.placeholder(dtype=tf.float32, shape=[BATCH_SIZE, num_features])
+        self.target_data = tf.placeholder(dtype=tf.int32, shape=[BATCH_SIZE])
+        self.dropout_prob = tf.placeholder(dtype=tf.float32, shape=[])
+
+        def makeGRUCells():
+            base_cell = tf.nn.rnn_cell.GRUCell(num_units=RNN_HIDDEN_SIZE, )
+            #layered_cell = tf.nn.rnn_cell.MultiRNNCell([base_cell] * NUM_LAYERS, state_is_tuple=False)
+            layered_cell = tf.nn.rnn_cell.MultiRNNCell(
+                [tf.nn.rnn_cell.GRUCell(num_units=RNN_HIDDEN_SIZE, ) for _ in range(NUM_LAYERS)], state_is_tuple=False)
+
+            attn_cell = tf.contrib.rnn.AttentionCellWrapper(cell=layered_cell, attn_length=ATTN_LENGTH,
+                                                            state_is_tuple=False)
+            return attn_cell
+
+        self.gru_cell = makeGRUCells()
+        self.zero_state = self.gru_cell.zero_state(1, tf.float32)
+
+        self.start_state = tf.placeholder(dtype=tf.float32, shape=[1, self.gru_cell.state_size])
+
+        with tf.variable_scope("ff", initializer=xavier_initializer(uniform=False)):
+            droped_input = tf.nn.dropout(self.input_data, keep_prob=self.dropout_prob)
+
+            layer_1 = tf.contrib.layers.fully_connected(
+                num_outputs=FIRST_LAYER_SIZE,
+                inputs=droped_input,
+
+            )
+            layer_2 = tf.contrib.layers.fully_connected(
+                num_outputs=RNN_HIDDEN_SIZE,
+                inputs=layer_1,
+
+            )
+
+        split_inputs = tf.reshape(droped_input, shape=[1, BATCH_SIZE, num_features],
+                                  name="reshape_l1")  # Each item in the batch is a time step, iterate through them
+        split_inputs = tf.unstack(split_inputs, axis=1, name="unpack_l1")
+        states = []
+        outputs = []
+        with tf.variable_scope("rnn", initializer=xavier_initializer(uniform=False)) as scope:
+            state = self.start_state
+            for i, inp in enumerate(split_inputs):
+                if i > 0:
+                    scope.reuse_variables()
+                output, state = self.gru_cell(inp, state)
+                states.append(state)
+                outputs.append(output)
+        self.end_state = states[-1]
+        outputs = tf.stack(outputs, axis=1)  # Pack them back into a single tensor
+        outputs = tf.reshape(outputs, shape=[BATCH_SIZE, RNN_HIDDEN_SIZE])
+        self.logits = tf.contrib.layers.fully_connected(
+            num_outputs=num_classes,
+            inputs=outputs,
+            activation_fn=None
+        )
+
+
+        with tf.variable_scope("loss"):
+            self.penalties = tf.reduce_sum([beta * tf.nn.l2_loss(var) for var in tf.trainable_variables()])
+
+            self.losses = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.logits, labels=self.target_data)
+            self.loss = tf.reduce_sum(self.losses + beta * self.penalties)
+
+        with tf.name_scope("train_step"):
+            opt = tf.train.AdamOptimizer(lr)
+            gvs = opt.compute_gradients(self.loss)
+            self.train_op = opt.apply_gradients(gvs, global_step=global_step)
+
+        with tf.name_scope("predictions"):
+            probs = tf.nn.softmax(self.logits)
+            self.predictions = tf.argmax(probs, 1)
+            correct_pred = tf.cast(tf.equal(self.predictions, tf.cast(self.target_data, tf.int64)), tf.float64)
+            self.accuracy = tf.reduce_mean(correct_pred)
+
+
+with tf.Graph().as_default():
+    model = RNNModel()
+    input_ = train[0]
+    target = train[1]
+    with tf.Session() as sess:
+        init = tf.initialize_all_variables()
+        sess.run([init])
+        loss = 2000
+
+        for e in range(NUM_EPOCHS):
+            state = sess.run(model.zero_state)
+            epoch_loss = 0
+            for batch in range(0, NUM_TRAIN_BATCHES):
+                start = batch * BATCH_SIZE
+                end = start + BATCH_SIZE
+                feed = {
+                    model.input_data: input_[start:end],
+                    model.target_data: target[start:end],
+                    model.dropout_prob: 0.5,
+                    model.start_state: state
+                }
+                _, loss, acc, state = sess.run(
+                    [
+                        model.train_op,
+                        model.loss,
+                        model.accuracy,
+                        model.end_state
+                    ]
+                    , feed_dict=feed
+                )
+                epoch_loss += loss
+
+            print('step - {0} loss - {1} acc - {2}'.format((e), epoch_loss, acc))
+        final_preds = np.array([])
+        for batch in range(0, NUM_VAL_BATCHES):
+            start = batch * BATCH_SIZE
+            end = start + BATCH_SIZE
+            feed = {
+                model.input_data: val[0][start:end],
+                model.target_data: val[1][start:end],
+                model.dropout_prob: 1,
+                model.start_state: state
+            }
+            acc, preds, state = sess.run(
+                [
+                    model.accuracy,
+                    model.predictions,
+                    model.end_state
+                ]
+                , feed_dict=feed
+            )
+            print(acc)
+            assert len(preds) == BATCH_SIZE
+            final_preds = np.concatenate((final_preds, preds), axis=0)
+
+print "*" * 20,"Train over","*" * 20
+
+Result['rnn_pred'] = final_preds
+Result['mod_rnn_prod'] = list(map(lambda x: -1 if x <5 else 0 if x==5 else 1,final_preds))
+Result['rnn_ret'] = Result.mod_rnn_prod*Result['return']
+
+print(confusion_matrix(Result['multi_class'],Result['rnn_pred']))
+print "-" * 20,"Confusion matrix","-" * 20
+
+print(classification_report(Result['class'],Result['mod_rnn_prod']))
+print "-" * 20,"Classification report","-" * 20
+
+print(confusion_matrix(Result['class'],Result['mod_rnn_prod']))
+print "-" * 20,"Confusion matrix2","-" * 20
+
+Res = (Result[-test_size:][['return','nn_ret','rnn_ret','pred_return']]).cumsum()
+print "-" * 20,"Return & nn_ret & rnn_ret & pred_return","-" * 20
+
+Res[0] =0
+Res.plot(figsize=(20,10))
+
+Res.columns =['Market Baseline','Simple Neural Newtwork','My Algo','Logistic Regression (simple ML)','Do Nothing(0)']
+Res.plot(figsize=(20,10),title="Performance of MarketVectors algo over 27 months compared with baselines")
+
+
+Res.columns
+Res.columns =['baseline','logistic_regression','feed_forward_net','rnn_net','do_nothing']
+Res.plot(figsize=(20,10))
+
+plot.draw()
